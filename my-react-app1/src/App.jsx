40fcbd90bd86c27a32b5ef68d40c3f9a0ee8f4e5
TYCHIEN1120 <tychien1120@outlook.com>
2024-12-04 10:38:06 +0800

    1204-1

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,50 +1,76 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from 'axios';
-
+import './App.css';
+import { CiUmbrella } from "react-icons/ci";
 function App() {
+    const [weatherList, setWeatherList] = useState([]);
 
     useEffect(() => {
         (async () => {
-            const data = await axios.get('./F-C0032-001.json');
+            const data = await axios.get('https://tychien1120.github.io/myReact7/json/F-C0032-001.json');
             const { location } = data.data.cwaopendata.dataset;
-            console.log(location);
+            setWeatherList(location);
+            // console.log(weatherList)
         })
     }, [])
 
 
     return (
-        <>
+        <div className="wrap">
             <h2>36小時天氣預報</h2>
-            <div classname="city">
-                <h3>台北市</h3>
+            <div className="container">
+                {
+                    weatherList.map((city) => {
+                        return (
+                            <div className="item" key={city.locationName}>
+                                <h3>{city.locationName}</h3>
+                                <div className="content">
+                                    {
+                                        city.weatherElement[0].time.map((time, index) => {
+                                            return (
+                                                <div className="item2" key={index}>
+                                                    {/* <p>4日</p> */}
+                                                    <p>
+                                                        {
+                                                            new Date(time.starTime).toLocaleString(undefined, {
+                                                                day: 'numeric'
+                                                            })
+                                                        }
+                                                    </p>
+
+                                                    <p>
+                                                        {
+                                                            new Date(time.starTime).toLocaleString(undefined, {
+                                                                hour: 'numeric',
+                                                                minute: 'numeric'
+                                                            })
+                                                        }
+                                                    </p>
+                                                    <p>
+                                                        {
+                                                            new Date(time.endTime).toLocaleString(undefined, {
+                                                                hour: 'numeric',
+                                                                minute: 'numeric'
+                                                            })
+                                                        }
+                                                    </p>
+                                                    <p><img src="./weatherIcon/陰短暫雨.svg" alt="" /></p>
+                                                    <p>陰短暫雨</p>
+                                                    <p><CiUmbrella />30%</p>
+                                                </div>
+                                            )
+                                        })
+                                    }
+
+
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
-            <div className="weather">
-                <article className="today">
-                    <header>
-                        <h3>2日</h3>
-                        <h4>上午6:00~下午6:00</h4>
-                    </header>
-                    <figure><img src="./weatherIcon.多雲時陰.svg." alt="" /></figure>
-                    <h4>晴時多雲</h4>
-                </article>
-                <article className="today2">
-                    <header>
-                        <h3>2日</h3>
-                        <h4>上午6:00~下午6:00</h4>
-                    </header>
-                    <figure><img src="./weatherIcon.多雲時陰.svg." alt="" /></figure>
-                    <h4>晴時多雲</h4>
-                </article>
-                <article className="tomorrow">
-                    <header>
-                        <h3>2日</h3>
-                        <h4>上午6:00~下午6:00</h4>
-                    </header>
-                    <figure><img src="./weatherIcon.多雲時陰.svg." alt="" /></figure>
-                    <h4>晴時多雲</h4>
-                </article>
-            </div>
-        </>
+        </div>
+
     )
 }
 export default App
